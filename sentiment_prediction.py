b6c88d14121eb9a1984662f0455a8fff28374e33
Mittesh <mitteshwaghule09@gmail.com>
2024-09-20 13:01:11 +0530

    sentiment classification done

--- sentiment_prediction.py
+++ sentiment_prediction.py
@@ -9,3 +9,39 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report
 
+mcd_data = pd.read_csv("data/McDonald_s_Reviews.csv", encoding="latin-1")
+
+# Data exploration
+# print(mcd_data.columns)
+# print(mcd_data.head(10))
+# print(mcd_data.sample)
+# print(mcd_data.info)
+
+# Sentiment score calculation
+sia = SentimentIntensityAnalyzer()
+
+# Performing sentiment analysis on each review
+sentiments = []
+for review in mcd_data['review']:
+    sentiment = sia.polarity_scores(review)
+    sentiments.append(sentiment)
+
+# print(sentiments)
+
+# Sentiment classification
+sentiment_labels = []
+for sentiment in sentiments:
+    compound_score = sentiment['compound']
+    if compound_score >= 0.05:
+        sentiment_labels.append("Positive")
+    elif compound_score <= 0.05:
+        sentiment_labels.append("Negative")
+    else:
+        sentiment_labels.append("Neutral")
+
+# print(sentiment_labels)
+
+# adding the sentiment labels to the dataframe
+mcd_data['sentiment'] = sentiment_labels
+
+# print(mcd_data[['review', 'sentiment']])
