f7a76812fccdeb304aa48f5e5c8cea5d7eeb2061
Mittesh <mitteshwaghule09@gmail.com>
2024-09-28 18:51:15 +0530

    Deploy #7

--- app.py
+++ app.py
@@ -15,4 +15,6 @@ def predict():
         return render_template('index.html', review=review, sentiment=sentiment)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the port from environment variable and default to 5000 for local development
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
