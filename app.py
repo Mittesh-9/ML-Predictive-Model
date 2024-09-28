89d9c068b21bc9c236fdc063ae4b74bff5338218
Mittesh <mitteshwaghule09@gmail.com>
2024-09-29 00:08:15 +0530

    Deploy #9

--- app.py
+++ app.py
@@ -19,3 +19,5 @@ if __name__ == '__main__':
     # Get the port from environment variable and default to 5000 for local development
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+    print(f"Running on port: {port}")
+
